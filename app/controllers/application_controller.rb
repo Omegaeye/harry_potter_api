d8b3d3b5b81c5e4458fe41a19838965929268561
Khoa Nguyen <khoa.nguyen@hotelengine.com>
2021-09-22 10:29:35 -0600

    Added oauth authorization and coversion to snake_case

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::API
+  prepend_before_action :underscore_params!
   before_action :doorkeeper_authorize!
 
-  def doorkeeper_authorize!
-    access_token = doorkeeper_token.token if doorkeeper_token.present?
-    token = Doorkeeper::AccessToken.find_by(token: access_token )
+  private
 
+  def doorkeeper_authorize!
+    access_token = doorkeeper_token[:token] if doorkeeper_token.present?
+    token = Doorkeeper::AccessToken.find_by(token: access_token )
+    
     unless token.present?
       doorkeeper_render_error
     end
@@ -12,5 +15,9 @@ class ApplicationController < ActionController::API
 
   def current_user
     @current_user ||= User.find_by(id: doorkeeper_token[:resource_owner_id])
+  end
+
+  def underscore_params!
+    params.deep_transform_keys!(&:underscore)
   end
 end
--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -5,10 +5,26 @@
 
 # Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
 ActiveSupport.on_load(:action_controller) do
-  wrap_parameters format: [:json]
+  wrap_parameters format: [:json] if respond_to?(:wrap_parameters)
+  include ::ActionController::ParamsNormalizer
 end
 
 # To enable root element in JSON for ActiveRecord objects.
 # ActiveSupport.on_load(:active_record) do
 #   self.include_root_in_json = true
 # end
+module ActionController
+  module ParamsNormalizer
+    extend ActiveSupport::Concern
+    require 'mutex_m'
+
+    def process_action(*args)
+      normalized_keys = request.request_parameters.keys
+      normalized_keys.each do |k|
+        request.request_parameters[k.underscore] = request.request_parameters[k]
+        request.request_parameters.delete(k) if k != k.underscore
+      end
+      super
+    end
+  end
+end
